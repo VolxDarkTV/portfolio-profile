9fa31b3d87c9a02ebf1c9d34a89ba87c41ef860d
Vincenzo <vincenzo.foti@outlook.it>
2023-02-26 19:18:14 +0100

    fixed carousel with a new library Swipersjs

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,16 +1,31 @@
 <script>
-    import { defineComponent } from 'vue';
-    import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel';
-    import 'vue3-carousel/dist/carousel.css';
-    export default defineComponent({
-        name: 'Basic',
-        components: {
-            Carousel,
-            Slide,
-            Pagination,
-            Navigation,
-        },
+    
+    import { Navigation, A11y } from 'swiper';
+    import { Swiper, SwiperSlide } from 'swiper/vue';
 
+    import 'swiper/css';
+    import 'swiper/css/navigation';
+    import 'swiper/css/pagination';
+    import 'swiper/css/scrollbar';
+    
+    export default {
+        components: {
+            Swiper,
+            SwiperSlide,
+        },
+        setup() {
+            const onSwiper = (swiper) => {
+                console.log(swiper);
+            };
+            const onSlideChange = () => {
+                console.log('slide change');
+            };
+            return {
+                onSwiper,
+                onSlideChange,
+                modules: [Navigation, A11y],
+            };
+        },
         data(){
             return{
                 myCarousel:[
@@ -47,8 +62,12 @@
             closePreview(){
                 this.preview = -1;
             }
+        },
+        mounted(){
+            
+            
         }
-    });
+    };
 </script>
 
 <template>
@@ -68,7 +87,7 @@
                 :alt="myCarousel[preview].img"
                 />
                 <div>
-                    <Carousel :items-to-show="6" :wrap-around="true">
+                    <!-- <Carousel :items-to-show="6" :wrap-around="true">
                         <Slide v-for="(slide, index) in myCarousel" :key="slide">
                             <img @click="setPreview(index)" :src="'./carousel/' + slide.img" alt="">
                         </Slide>
@@ -76,10 +95,11 @@
                         <template #addons>
                           <Navigation class="my_navigation_preview"/>
                         </template>
-                    </Carousel>
+                    </Carousel> -->
                 </div>
             </div>
-            <Carousel :items-to-show="1.5" :wrap-around="true" :autoplay="5000">
+
+            <!-- <Carousel :items-to-show="1.5" :wrap-around="true" :autoplay="5000">
                 <Slide v-for="(slide, index) in myCarousel" :key="slide">
                     <img @click="setPreview(index)" :src="'./carousel/' + slide.img" alt="">
                     
@@ -88,7 +108,23 @@
                 <template #addons>
                   <Navigation />
                 </template>
-            </Carousel>
+            </Carousel> -->
+
+            <swiper
+                :modules="modules"
+                :slides-per-view="1.5"
+                :space-between="20"
+                navigation
+                :loop="true"
+                @swiper="onSwiper"
+                @slideChange="onSlideChange"
+                :centeredSlides="true"
+            >
+                <swiper-slide v-for="(slide, index) in myCarousel" :key="slide">
+                    <img @click="setPreview(index)" :src="'./carousel/' + slide.img" alt="">
+                </swiper-slide>
+                
+            </swiper>
 
         </div>
 
@@ -98,50 +134,86 @@
 
 <style lang="scss">
 @use '../styles/general.scss' as *;
-.carousel__item {
-    min-height: 200px;
-    width: 100%;
-    background-color: var(--vc-clr-primary);
-    color: var(--vc-clr-white);
-    font-size: 20px;
-    border-radius: 8px;
+// .carousel__item {
+//     min-height: 200px;
+//     width: 100%;
+//     background-color: var(--vc-clr-primary);
+//     color: var(--vc-clr-white);
+//     font-size: 20px;
+//     border-radius: 8px;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+// }
+
+// .carousel__slide {
+//     padding: 0px 10px;
+// }
+
+// .carousel__prev{
+//     transform: rotateY(-10deg) scale(0.95);
+
+//     position: absolute;
+//     top: 42%;
+//     left: 5%;
+//     padding: 10px;
+//     border-radius: 50%;
+//     background-color: $brand_third;
+//     color: $brand_fourth;
+// }
+// .carousel__next {
+//     transform: rotateY(10deg) scale(0.95);
+
+//     position: absolute;
+//     top: 42%;
+//     right: 5%;
+//     padding: 10px;
+//     border-radius: 50%;
+//     background-color: $brand_third;
+//     color: $brand_fourth;
+// }
+
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+// Prev & Next
+.swiper-button-next{
     display: flex;
     justify-content: center;
     align-items: center;
-}
-
-.carousel__slide {
-    padding: 0px 10px;
-}
-
-.carousel__prev{
-    transform: rotateY(-10deg) scale(0.95);
-
-    position: absolute;
-    top: 42%;
-    left: 5%;
-    padding: 10px;
+    
+    width: 50px;
+    height: 50px;
+    padding: 20px 20px;
     border-radius: 50%;
-    background-color: $brand_third;
     color: $brand_fourth;
+    background-color: $brand_third;
 }
-.carousel__next {
-    transform: rotateY(10deg) scale(0.95);
-
-    position: absolute;
-    top: 42%;
-    right: 5%;
-    padding: 10px;
+.swiper-button-next::after{
+    font-size: 20px;
+}
+.swiper-button-prev{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    width: 50px;
+    height: 50px;
+    padding: 20px 20px;
     border-radius: 50%;
-    background-color: $brand_third;
     color: $brand_fourth;
+    background-color: $brand_third;
+}
+.swiper-button-prev::after{
+    font-size: 20px;
 }
 
 // preview
 .preview-container {
     z-index: 999;
     position: fixed;
-    top: 20%;
+    top: 10%;
     left: 50%;
     transform: translateX(-50%);
     background-color: #000;
@@ -150,8 +222,8 @@
 }
 
 .preview-image {
-    max-width: 1200px;
-    max-height: 1200px;
+    width: 90vw;
+    // max-height: 1200px;
 }
 
 .my_close{
