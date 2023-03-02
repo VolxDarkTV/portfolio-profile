a9f7b4acd5d274e8271786c27cb0372f9db8d60f
Vincenzo <vincenzo.foti@outlook.it>
2023-03-02 12:33:00 +0100

    fixed experience order and cv

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -12,48 +12,8 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="main-timeline">
-                            <!-- 2020 -->
-                            <div class="timeline" data-aos="fade-right" data-aos-delay="100">
-                                <a href="#" class="timeline-content">
-                                    <div class="timeline-year">2020</div>
-                                    <div class="timeline-icon">
-                                        <font-awesome-icon icon="fa-solid fa-store" />
-                                    </div>
-                                    <h3 class="title fw-bold my_third">Ristorante</h3>
-                                    <p class="description">
-                                        A 16 anni ho lavorato in un ristorante come <span class="fw-bold">addetto sala</span> / <span class="fw-bold">addetto cucina</span> / <span class="fw-bold">addetto bar</span>. Le mie responsabilità crescevano sempre più con il passare del tempo, come le mie skills acquisite aumentavano con il passare del tempo. 
-                                    </p>
-                                </a>
-                            </div>
-                            <!-- 2021 -->
-                            <div class="timeline" data-aos="fade-left" data-aos-delay="200">
-                                <a href="#" class="timeline-content">
-                                    <div class="timeline-year">2021</div>
-                                    <div class="timeline-icon">
-                                        <font-awesome-icon icon="fa-solid fa-store" />
-                                    </div>
-                                    <h3 class="title fw-bold my_third">Ristorante</h3>
-                                    <p class="description">
-                                        A 17 anni ho lavorato in un ristorante come <span class="fw-bold">addetto sala</span> / <span class="fw-bold">addetto cucina</span> / <span class="fw-bold">addetto bar</span>. Le mie responsabilità crescevano sempre più con il passare del tempo, come le mie skills acquisite aumentavano con il passare del tempo. 
-                                    </p>
-                                </a>
-                            </div>
-                            <!-- 2022 -->
-                            <div class="timeline" data-aos="fade-right" data-aos-delay="300">
-                                <a href="#" class="timeline-content">
-                                    <div class="timeline-year">2022</div>
-                                    <div class="timeline-icon">
-                                        <font-awesome-icon icon="fa-solid fa-cow" />
-                                    </div>
-                                    <h3 class="title fw-bold my_third">Macelleria</h3>
-                                    <p class="description">
-                                        Ho lavorato in questa macelleria all'ingrosso, in cui avevo il compito di 
-                                        <span class="fw-bold">assemblare</span> / <span class="fw-bold">porzionare i tagli</span> / <span class="fw-bold">confezionare</span> e <span class="fw-bold">spedire</span> in reparto vendita.
-                                    </p>
-                                </a>
-                            </div>
                             <!-- 2023 -->
-                            <div class="timeline" data-aos="fade-left" data-aos-delay="400">
+                            <div class="timeline" data-aos="fade-left" data-aos-delay="100">
                                 <a href="#" class="timeline-content">
                                     <div class="timeline-year">2023</div>
                                     <div class="timeline-icon">
@@ -71,6 +31,50 @@
                                     </p>
                                 </a>
                             </div>
+                            
+                            <!-- 2022 -->
+                            <div class="timeline" data-aos="fade-right" data-aos-delay="200">
+                                <a href="#" class="timeline-content">
+                                    <div class="timeline-year">2022</div>
+                                    <div class="timeline-icon">
+                                        <font-awesome-icon icon="fa-solid fa-cow" />
+                                    </div>
+                                    <h3 class="title fw-bold my_third">Macelleria</h3>
+                                    <p class="description">
+                                        Ho lavorato in questa macelleria all'ingrosso, in cui avevo il compito di 
+                                        <span class="fw-bold">assemblare</span> / <span class="fw-bold">porzionare i tagli</span> / <span class="fw-bold">confezionare</span> e <span class="fw-bold">spedire</span> in reparto vendita.
+                                    </p>
+                                </a>
+                            </div>
+
+                            <!-- 2021 -->
+                            <div class="timeline" data-aos="fade-left" data-aos-delay="300">
+                                <a href="#" class="timeline-content">
+                                    <div class="timeline-year">2021</div>
+                                    <div class="timeline-icon">
+                                        <font-awesome-icon icon="fa-solid fa-store" />
+                                    </div>
+                                    <h3 class="title fw-bold my_third">Ristorante</h3>
+                                    <p class="description">
+                                        A 17 anni ho lavorato in un ristorante come <span class="fw-bold">addetto sala</span> / <span class="fw-bold">addetto cucina</span> / <span class="fw-bold">addetto bar</span>. Le mie responsabilità crescevano sempre più con il passare del tempo, come le mie skills acquisite aumentavano con il passare del tempo. 
+                                    </p>
+                                </a>
+                            </div>
+                            
+                            <!-- 2020 -->
+                            <div class="timeline" data-aos="fade-right" data-aos-delay="400">
+                                <a href="#" class="timeline-content">
+                                    <div class="timeline-year">2020</div>
+                                    <div class="timeline-icon">
+                                        <font-awesome-icon icon="fa-solid fa-store" />
+                                    </div>
+                                    <h3 class="title fw-bold my_third">Ristorante</h3>
+                                    <p class="description">
+                                        A 16 anni ho lavorato in un ristorante come <span class="fw-bold">addetto sala</span> / <span class="fw-bold">addetto cucina</span> / <span class="fw-bold">addetto bar</span>. Le mie responsabilità crescevano sempre più con il passare del tempo, come le mie skills acquisite aumentavano con il passare del tempo. 
+                                    </p>
+                                </a>
+                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -106,7 +110,7 @@
 .main-timeline .timeline-content:hover{ text-decoration: none; }
 .main-timeline .timeline-content:before{
     content: '';
-    background-color: #00A7FB;
+    background-color: $brand_fourth;
     height: 100%;
     width: 120px;
     position: absolute;
@@ -114,7 +118,7 @@
     top: 0;
 }
 .main-timeline .timeline-year{
-    color: #00A7FB;
+    color: $brand_fourth;
     font-size: 40px;
     font-weight: 700;
     line-height: 43px;
@@ -168,8 +172,8 @@
 .main-timeline .timeline:nth-child(4n+2) .timeline-year{ color: $brand_primary; }
 .main-timeline .timeline:nth-child(4n+3) .timeline-content:before{ background-color: $brand_secondary; }
 .main-timeline .timeline:nth-child(4n+3) .timeline-year{ color: $brand_secondary; }
-.main-timeline .timeline:nth-child(4n+4) .timeline-content:before{ background-color: $brand_fourth; }
-.main-timeline .timeline:nth-child(4n+4) .timeline-year{ color: $brand_fourth; }
+.main-timeline .timeline:nth-child(4n+4) .timeline-content:before{ background-color: $brand_third; }
+.main-timeline .timeline:nth-child(4n+4) .timeline-year{ color: $brand_third; }
 @media screen and (max-width:767px){
     .main-timeline .timeline,
     .main-timeline .timeline:nth-child(even){
